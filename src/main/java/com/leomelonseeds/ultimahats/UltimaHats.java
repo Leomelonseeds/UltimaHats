c1385d156167781047f69deae4e7735e67f9ef1e
Leomelonseeds <leonardfang15@gmail.com>
2022-11-05 18:04:07 -0700

    Continue plugin setup, add SQL features

--- src/main/java/com/leomelonseeds/ultimahats/UltimaHats.java
+++ src/main/java/com/leomelonseeds/ultimahats/UltimaHats.java
@@ -1,29 +1,70 @@
 package com.leomelonseeds.ultimahats;
 
+import java.io.File;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.leomelonseeds.ultimahats.db.SQLManager;
 import com.leomelonseeds.ultimahats.inv.InventoryManager;
 
 public class UltimaHats extends JavaPlugin {
     
     public static UltimaHats plugin;
     private InventoryManager invs;
+    private SQLManager sqlManager;
     
     @Override
     public void onEnable() {
         
+        log("Enabling UltimaHats...");
         plugin = this;
-        
         invs = new InventoryManager();
+        
+        log("Saving config files");
+        saveDefaultConfig();
+        saveIfNotPresent("hats.yml");
+        log("Config files saved!");
+        
+        log("Initializing database...");
+        setupDatabase();
+        log("Database setup complete.");
+        
+        
         
     }
     
+    private void setupDatabase() {
+        sqlManager = new SQLManager(this);
+        log("Testing SQL connection...");
+        if (!sqlManager.testConnection()) {
+            log("The plugin cannot function without the database. Shutting down now...");
+            this.getPluginLoader().disablePlugin(this);
+        }
+        log("SQL connection test complete.");
+
+        log("Setting up default tables...");
+        sqlManager.setupTables();
+    }
+    
+    /**
+     * Save a resource if it is not present
+    *
+    * @param resourcePath the path to the resource
+    */
+   private void saveIfNotPresent(String resourcePath) {
+       File file = new File(getDataFolder(), resourcePath);
+       if (!file.exists()) {
+           saveResource(resourcePath, false);
+       }
+   }
+    
     @Override
     public void onDisable() {
-        
+        log("Closing database...");
+        sqlManager.close();
+        log("Have a nice day!");
     }
     
     public static UltimaHats getPlugin() {
@@ -31,7 +72,11 @@ public class UltimaHats extends JavaPlugin {
     }
     
     public void log(String message) {
-        Bukkit.getLogger().log(Level.INFO, "[UltimaHats]" + message);
+        Bukkit.getLogger().log(Level.INFO, "[UltimaHats] " + message);
+    }
+    
+    public SQLManager getSQL() {
+        return sqlManager;
     }
     
     public InventoryManager getInvs() {
--- src/main/java/com/leomelonseeds/ultimahats/util/ItemUtils.java
+++ src/main/java/com/leomelonseeds/ultimahats/util/ItemUtils.java
@@ -1,5 +1,19 @@
 package com.leomelonseeds.ultimahats.util;
 
+import org.bukkit.entity.Player;
+
+import com.leomelonseeds.ultimahats.UltimaHats;
+
 public class ItemUtils {
+    
+    public static void applyHat(Player player) {
+        UltimaHats.getPlugin().getSQL().getHat(player.getUniqueId(), result -> {
+            if (result == null) {
+                return;
+            }
+            
+            String hat = (String) result;
+        });
+    }
 
 }
