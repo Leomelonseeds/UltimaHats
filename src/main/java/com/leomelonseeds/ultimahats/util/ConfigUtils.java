0703202235cf3dc9b78d28e573e87e3159f9bab3
Leomelonseeds <leonardfang15@gmail.com>
2022-11-05 22:30:30 -0700

    Work on config format and SQL additions

--- src/main/java/com/leomelonseeds/ultimahats/util/ConfigUtils.java
+++ src/main/java/com/leomelonseeds/ultimahats/util/ConfigUtils.java
@@ -1,5 +1,42 @@
 package com.leomelonseeds.ultimahats.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.leomelonseeds.ultimahats.UltimaHats;
+
 public class ConfigUtils {
+
+    // Map of open cached config files
+    private static Map<String, FileConfiguration> configCache = new HashMap<>();
+    
+    /**
+     * Get config file from default folder
+     * 
+     * @param configName
+     * @return
+     */
+    public static FileConfiguration getConfigFile(String configName) {
+        // Check for config file in cache
+        if (configCache.containsKey(configName)) {
+            return configCache.get(configName);
+        }
+
+        File file = new File(UltimaHats.getPlugin().getDataFolder().toString(), configName);
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            config.load(file);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        configCache.put(configName, config);
+        return config;
+    }
 
 }
--- src/main/java/com/leomelonseeds/ultimahats/util/ItemUtils.java
+++ src/main/java/com/leomelonseeds/ultimahats/util/ItemUtils.java
@@ -1,11 +1,24 @@
 package com.leomelonseeds.ultimahats.util;
 
-import org.bukkit.entity.Player;
+import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import com.leomelonseeds.ultimahats.UltimaHats;
 
 public class ItemUtils {
     
+    /**
+     * Applies a hat to a player
+     * 
+     * @param player
+     */
     public static void applyHat(Player player) {
         UltimaHats.getPlugin().getSQL().getHat(player.getUniqueId(), result -> {
             if (result == null) {
@@ -14,6 +27,42 @@ public class ItemUtils {
             
             String hat = (String) result;
         });
+    }
+    
+    /**
+     * Removes player's currently equipped hat, if there is one
+     * 
+     * @param player
+     */
+    public static void removeHat(Player player) {
+        
+    }
+    
+    /**
+     * Checks the necessary conditions to see if a player owns a hat
+     * 
+     * @param player
+     * @param hat
+     * @return
+     */
+    public static boolean ownsHat(Player player, String hat) {
+        return false;
+    }
+    
+    /**
+     * Make a skull from a base 64 string
+     * 
+     * @param base64EncodedString
+     * @return
+     */
+    public ItemStack makeSkull(String base64EncodedString) {
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) skull.getItemMeta();
+        PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID());
+        profile.setProperty(new ProfileProperty("textures", base64EncodedString));
+        meta.setPlayerProfile(profile);
+        skull.setItemMeta(meta);
+        return skull;
     }
 
 }
